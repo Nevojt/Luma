1d2c5b342464dbc5fea81d30525995f3ad92153b
Nevojt <nevojt@gmail.com>
2024-11-03 20:37:15 +0100

    commit

---
+++ LumaProject/day_5/day_5.py
@@ -0,0 +1,17 @@
+
+# print("Hello this is the greate world")
+#
+# question = input("How do you think, what is true? ")
+#
+# print("Your answer: " + question)
+#
+# print("Hello I am calculator.")
+#
+# num1 = int(input("What is first nuber? "))
+# # action = input("What is action? ")
+# num2 = int(input("What is second number? "))
+#
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+# print(num1 / num2)
