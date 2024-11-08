9a404d5eb3576cb5da2d0d39ea70a20d4472b3e9
Nevojt <nevojt@gmail.com>
2024-11-08 19:24:45 +0100

    Update foo

--- LumaProject/day_3/day_3.py
+++ LumaProject/day_3/day_3.py
@@ -2,5 +2,7 @@ hello: str = "Hello"
 world: str = "world!"
 
 user: str = input("Enter your username ")
+def foo(user:str):
+    print(f"{hello} {user}! {world}")
 
-print(f"{hello} {user}! {world}")
+foo(user)
