32af54eb752ab2dd81e7a8e18cc2b929cb3f6a81
Nevojt <nevojt@gmail.com>
2024-11-09 12:49:09 +0100

    day_6 update

---
+++ LumaProject/day_6/day_6.py
@@ -0,0 +1,40 @@
+# name_big = "JoN"
+# name_little = "jon"
+#
+# username = "SeRgiJ"
+# us = username.lower()
+# up = username.upper()
+#
+# print("lower ", us)
+# print("upper ", up)
+
+# name: str = input("User name lower: ").upper()
+#
+# last_name: str = input("Last name: ").lower()
+#
+# print(name + " " + last_name)
+# print(name == last_name)
+# print(name != last_name)
+
+first_number: int = 10 + 10
+last_number: int = 20
+
+print(first_number == last_number)
+print(first_number != last_number)
+print(first_number >= last_number)
+print(first_number <= last_number)
+
+
+
+
+
+
+
+#
+# print(name_big.lower())
+# print(name_little)
+#
+# result = name_big == name_little
+# print(result)
+
+
