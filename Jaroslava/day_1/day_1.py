bcf4d18e906083e690d62bfecec06e6d0f1754ac
Nevojt <nevojt@gmail.com>
2024-11-16 12:51:56 +0100

    Luma

---
+++ Jaroslava/day_1/day_1.py
@@ -0,0 +1,10 @@
+
+# print("Hello World!!")
+# print("12()" "+" "12")
+# print("I am are 1988 + 10 years old")
+# print(12 + 12)
+#
+# print("Dmytro" + " " + "World" + " " + "Hello")
+
+name = "Dmytro"
+print("Hello " + name)
