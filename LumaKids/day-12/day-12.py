0738dd5b77bc95f41e267c7344be8fe129520697
Nevojt <nevojt@gmail.com>
2025-01-12 12:05:10 +0100

    Luma

---
+++ LumaKids/day-12/day-12.py
@@ -0,0 +1,23 @@
+# import random
+# from random import randint, random, shuffle
+#
+# list_1 = [1, 2, 3, 4, 5, 6, 7, 8]
+#
+# num = randint(0, 100)
+# num2 = random()
+# num3 = shuffle(list_1)
+#
+# print(num)
+# print(num2)
+#
+# print(list_1)
+
+# from datetime import datetime
+#
+# print(datetime.now())
+# print(datetime.date(datetime.now()))
+
+import pyfiglet
+
+result = pyfiglet.figlet_format("Yes")
+print(result)
---
+++ LumaKids/day-13/calkulate.py
@@ -0,0 +1,24 @@
+hader = r'''
+           _            _       _
+  ___ __ _| | ___ _   _| | __ _| |_ ___  _ __
+ / __/ _` | |/ __| | | | |/ _` | __/ _ \| '__|
+| (_| (_| | | (__| |_| | | (_| | || (_) | |
+ \___\__,_|_|\___|\__,_|_|\__,_|\__\___/|_|
+'''
+
+print(hader)
+
+print("This is calculater!!!\n")
+#
+# num1 = float(input("Enter first number: "))
+# option = input("Enter operator (+, -, *, /): ")
+# num2 = float(input("Enter second number: "))
+#
+#
+# if option == "+":
+#     result = num1 + num2
+#     print(f"{num1} + {num2} = {result}")
+#
+# elif option == "-":
+#     result = num1 - num2
+#     print(f"{num1} - {num2} = {result}")
